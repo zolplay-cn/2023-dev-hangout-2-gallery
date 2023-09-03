9bcfee58726ef4e7e0da8558e566964441e93cd7
Cali <hi@cali.so>
2023-09-04 01:59:02 +0800

    fix: tweet intent

--- components/SharedModal.tsx
+++ components/SharedModal.tsx
@@ -128,7 +128,7 @@ export default function SharedModal({
                   </a>
                 ) : (
                   <a
-                    href={`https://twitter.com/intent/tweet?text=Check%20out%20this%20pic%20from%202023佐玩首届开发者交流会照片%0A%0Ahttps://hangout-1-pics.zolplay.com/p/${index}`}
+                    href={`https://twitter.com/intent/tweet?text=看看这张照片——2023佐玩首届开发者交流会%0A%0Ahttps://hangout-1-pics.zolplay.com/p/${index}`}
                     className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
                     target="_blank"
                     title="Open fullsize version"
